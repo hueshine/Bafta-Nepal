37da04bd1ec60363b6c7c06117b7f1d217b1909b
yogesh-karki <yogesh@hueshine.com>
2022-01-05 14:58:16 +0545

    responsive + design

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 
 $('.events-slider').owlCarousel({
-    stagePadding: 170,
+    
     loop:true,
     margin:10,
     nav:true,
     responsive:{
         0:{
-            items:1
+            items:1,
+            stagePadding: 0,
         },
         600:{
-            items:1
+            items:1,
+            stagePadding: 0,
         },
         1000:{
-            items:1
+            items:1,
+            stagePadding: 350,
         }
     }
 })
