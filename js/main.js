8ca30038bf7985af97723ad323cceac8b3d86a84
neu-astha <neuastha12@gmail.com>
2022-01-06 16:35:17 +0545

    change

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-$('.events-slider').owlCarousel({
+$('.client-slider').owlCarousel({
     
     loop:true,
     margin:10,
@@ -15,7 +15,31 @@ $('.events-slider').owlCarousel({
         },
         1000:{
             items:1,
-            stagePadding: 350,
+            stagePadding: 250,
+        }
+    }
+})
+
+$('.news-slider').owlCarousel({
+    
+    center: true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1,
+            stagePadding: 20
+            
+        },
+        600:{
+            items:2,
+            
+        },
+        1000:{
+            items:3,
+            stagePadding: 120,
+            
         }
     }
 })
